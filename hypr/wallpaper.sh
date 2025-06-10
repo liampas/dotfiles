d46e83f9c727e23b33e116f473cac8f5f301e83b
liampas <liam.plamondon@gmail.com>
2025-06-10 18:32:14 -0400

    added usefull variables for colors wal found

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -11,6 +11,25 @@ hyprctl hyprpaper reload ,"$WALLPAPER"
 
 wal -i $WALLPAPER
 
+c1=$(grep "#" ~/.cache/wal/colors | sed -n '1p')
+c2=$(grep "#" ~/.cache/wal/colors | sed -n '2p')
+c3=$(grep "#" ~/.cache/wal/colors | sed -n '3p')
+c4=$(grep "#" ~/.cache/wal/colors | sed -n '4p')
+c5=$(grep "#" ~/.cache/wal/colors | sed -n '5p')
+c6=$(grep "#" ~/.cache/wal/colors | sed -n '6p')
+c7=$(grep "#" ~/.cache/wal/colors | sed -n '7p')
+c8=$(grep "#" ~/.cache/wal/colors | sed -n '8p')
+c9=$(grep "#" ~/.cache/wal/colors | sed -n '9p')
+c10=$(grep "#" ~/.cache/wal/colors | sed -n '10p')
+c11=$(grep "#" ~/.cache/wal/colors | sed -n '11p')
+c12=$(grep "#" ~/.cache/wal/colors | sed -n '12p')
+c13=$(grep "#" ~/.cache/wal/colors | sed -n '13p')
+c14=$(grep "#" ~/.cache/wal/colors | sed -n '14p')
+c15=$(grep "#" ~/.cache/wal/colors | sed -n '15p')
+c16=$(grep "#" ~/.cache/wal/colors | sed -n '16p')
+
+
+
 #BORDER=$(xrdb -query | grep '\*color1' | head -n1 | sed 's/.*#//')
 #BORDER=$(grep '\*color1' ~/.Xresources | head -n1 | sed 's/.*#//')
 
