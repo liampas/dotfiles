3d3048df5f1df11baeca066a71c2780fec23a1ec
liampas <liam.plamondon@gmail.com>
2025-06-24 17:29:05 -0400

    added the notification toggle to the settings

--- hypr/quick-settings.sh
+++ hypr/quick-settings.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-
+# variable for floating mode
 floatingmode=$(grep 'floatingmode' ~/.config/hypr/toggles.state | cut -d '=' -f 2)
 
 if [ $floatingmode -eq 0 ]; then
     floatingmode2=$"tilling"
-
     else
-
     floatingmode2=$"floating"
-
 fi
 
+# variable for day mode
 if pgrep -x "hyprsunset" > /dev/null; then
     night=$"Day"
-
     else
-
     night=$"Night"
+fi
 
+
+# variable for notification mode
+DND=$(swaync-client -D)
+
+if [ "$DND" = "true" ]; then
+  DND=$"Off"
+else
+  DND=$"On"
 fi
 
 
@@ -62,14 +67,16 @@ floating () { \
 
 
 timetochoose() { \
-    choice=$(printf "window mode: $floatingmode2\\nRandom Wallpaper\\nStart Waybar\\n$night Mode" | dmenu -c -l 20 -p "Quick Settings: ")
+    choice=$(printf "window mode: $floatingmode2\\nRandom Wallpaper\\nStart Waybar\\n$night Mode\\nNotifications $DND" | dmenu -c -l 20 -p "Quick Settings: ")
     case "$choice" in
         "window mode: floating") floating;;
         "window mode: tilling") tilling;;
         'Random Wallpaper') ~/.config/hypr/wallpaper.sh;;
         'Start Waybar') ~/.config/hypr/reload-waybar.sh;;
         'Night Mode') ~/.config/hypr/night-mode.sh;;
-        'Day Mode') killall hyprsunset
+        'Day Mode') killall hyprsunset;;
+        'Notifications Off') swaync-client -df;;
+        'Notifications On') swaync-client -dn
     esac
 }
 
