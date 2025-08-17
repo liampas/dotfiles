a0af86150e759f24b68936399b3aae7e1c0db692
liampas <liam.plamondon@gmail.com>
2025-08-17 09:46:02 -0400

    added some missing configs

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,68 @@
+vim.g.mapleader = ";"
+
+
+
+--insert time/date
+vim.api.nvim_set_keymap('n', '<leader>d', [[<cmd>lua insert_date()<CR>]], { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>t', [[<cmd>lua insert_time()<CR>]], { noremap = true, silent = true })
+
+function insert_date()
+local date = os.date("%y-%m-%d (%H:%M)")
+vim.api.nvim_put({date}, 'c', true, true)
+end
+
+function insert_time()
+local date = os.date("(%H:%M)")
+vim.api.nvim_put({date}, 'c', true, true)
+end
+
+
+
+vim.o.number = true
+vim.cmd.syntax("on")
+vim.o.hlsearch = true
+
+
+--- Plugins ---
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+
+Plug('nvim-treesitter/nvim-treesitter')
+Plug('nvim-tree/nvim-tree.lua')
+Plug('uZer/pywal16.nvim', { [ 'as' ] = 'pywal16' })
+
+vim.call('plug#end')
+
+
+-- NvimFileTree --
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+})
